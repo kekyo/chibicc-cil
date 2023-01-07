9b88b6f44b1c5bf184bef20dec9c4d8ddb78f7a1
Kouji Matsui <k@kekyo.net>
2023-01-07 23:09:16 +0900

    .NET: Add "while" statement

--- codegen.c
+++ codegen.c
@@ -94,7 +94,8 @@ static void gen_stmt(Node *node) {
   }
   case ND_FOR: {
     int c = count();
-    gen_stmt(node->init);
+    if (node->init)
+      gen_stmt(node->init);
     printf("_L_begin_%d:\n", c);
     if (node->cond) {
       gen_expr(node->cond);
