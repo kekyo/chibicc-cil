f4ffd5dd85fcc6af82e34a80e9c307cf00c11708
Kouji Matsui <k@kekyo.net>
2023-01-08 20:21:09 +0900

    .NET: Add a representative node to each Node to improve error messages

--- codegen.c
+++ codegen.c
@@ -13,7 +13,7 @@ static void gen_addr(Node *node) {
     return;
   }
 
-  error("not an lvalue");
+  error_tok(node->tok, "not an lvalue");
 }
 
 static void gen_expr(Node *node) {
@@ -72,7 +72,7 @@ static void gen_expr(Node *node) {
     return;
   }
 
-  error("invalid expression");
+  error_tok(node->tok, "invalid expression");
 }
 
 static void gen_stmt(Node *node) {
@@ -124,7 +124,7 @@ static void gen_stmt(Node *node) {
     return;
   }
 
-  error("invalid statement");
+  error_tok(node->tok, "invalid statement");
 }
 
 // Assign offsets to local variables.
