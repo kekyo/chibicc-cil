edcc82e2bb47b3e2798ed2a9ae6a7106e80f7e01
Kouji Matsui <k@kekyo.net>
2023-01-12 22:27:11 +0900

    .NET: Support zero-arity function calls

---
+++ test2.s
@@ -0,0 +1,17 @@
+.assembly tmp2
+{
+}
+.class public auto ansi abstract sealed beforefieldinit chibicc
+  extends [mscorlib]System.Object
+{
+  .method public hidebysig static int32 ret3() cil managed
+  {
+    ldc.i4.3
+    ret
+  }
+  .method public hidebysig static int32 ret5() cil managed
+  {
+    ldc.i4.5
+    ret
+  }
+}
