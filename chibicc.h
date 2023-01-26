c136b42e8556177b163f2fc024d99a6865038dab
Kouji Matsui <k@kekyo.net>
2023-01-26 21:18:24 +0900

    .NET: [GNU] Add statement expression
    This is a GNU C extension but will be useful for writing tests.

--- chibicc.h
+++ chibicc.h
@@ -100,6 +100,7 @@ typedef enum {
   ND_BLOCK,     // { ... }
   ND_FUNCALL,   // Function call
   ND_EXPR_STMT, // Expression statement
+  ND_STMT_EXPR, // Statement expression
   ND_VAR,       // Variable
   ND_NUM,       // Integer
 } NodeKind;
@@ -122,7 +123,7 @@ struct Node {
   Node *init;
   Node *inc;
 
-  // Block
+  // Block or statement expression
   Node *body;
 
   // Function call
