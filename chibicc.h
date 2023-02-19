fce7f8b5ad5251e17d40f996aba13fd4d69dfb10
Kouji Matsui <k@kekyo.net>
2023-02-20 00:14:07 +0900

    .NET: Add void type

--- chibicc.h
+++ chibicc.h
@@ -158,6 +158,7 @@ Obj *parse(Token *tok);
 //
 
 typedef enum {
+  TY_VOID,
   TY_CHAR,
   TY_SHORT,
   TY_INT,
@@ -206,6 +207,8 @@ struct Member {
   Token *name;
   Node *offset;
 };
+
+extern Type *ty_void;
 
 extern Type *ty_char;
 extern Type *ty_short;
