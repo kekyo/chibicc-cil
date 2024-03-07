92e25b407c90fea5861fbf1960667ddac9f2344f
Kouji Matsui <k@kekyo.net>
2024-03-07 21:26:09 +0900

    .NET: [GNU] Add __FUNCTION__

--- parse.c
+++ parse.c
@@ -2440,6 +2440,10 @@ static Token *function(Token *tok, Type *basety, VarAttr *attr) {
   push_scope("__func__")->var =
     new_string_literal(fn->name, array_of(ty_char, strlen(fn->name) + 1, tok));
 
+  // [GNU] __FUNCTION__ is yet another name of __func__.
+  push_scope("__FUNCTION__")->var =
+    new_string_literal(fn->name, array_of(ty_char, strlen(fn->name) + 1, tok));
+
   tok = skip(tok, "{");
 
   fn->body = compound_stmt(&tok, tok);
