8128576384935955e50f00ff5ab476c3e284495f
Kouji Matsui <k@kekyo.net>
2024-03-25 19:48:08 +0900

    .NET: Allow multibyte UTF-8 character in identifier

--- chibicc.h
+++ chibicc.h
@@ -456,6 +456,8 @@ int64_t get_by_integer(Node *node);
 
 int encode_utf8(char *buf, uint32_t c);
 uint32_t decode_utf8(char **new_pos, char *p);
+bool is_ident1(uint32_t c);
+bool is_ident2(uint32_t c);
 
 //
 // codegen.c
